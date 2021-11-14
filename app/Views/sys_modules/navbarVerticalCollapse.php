b04371df5583e9b2135f68027039f2eb23f62ffc
José L. Gutiérrez <gutiecuador@gmail.com>
2021-11-14 11:54:54 -0500

    split code navbars

---
+++ app/Views/sys_modules/navbarVerticalCollapse.php
@@ -0,0 +1,26 @@
+<div class="collapse navbar-collapse" id="navbarVerticalCollapse">
+    <div class="navbar-vertical-content scrollbar">
+        <ul class="navbar-nav flex-column mb-3" id="navbarVerticalNav">
+        <li class="nav-item">
+            <!-- parent pages--><a class="nav-link dropdown-indicator" href="#dashboard" role="button" data-bs-toggle="collapse" aria-expanded="true" aria-controls="dashboard">
+            <div class="d-flex align-items-center"><span class="nav-link-icon"><span class="fas fa-chart-pie"></span></span><span class="nav-link-text ps-1">Dashboard</span>
+            </div>
+            </a>
+            <ul class="nav collapse show" id="dashboard">
+            <li class="nav-item"><a class="nav-link active" href="index.html" aria-expanded="false">
+                <div class="d-flex align-items-center"><span class="nav-link-text ps-1">Métricas</span>
+                </div>
+                </a>
+                <!-- more inner pages-->
+            </li>
+            <li class="nav-item"><a class="nav-link" href="dashboard/analytics.html" aria-expanded="false">
+                <div class="d-flex align-items-center"><span class="nav-link-text ps-1">Tablas</span>
+                </div>
+                </a>
+                <!-- more inner pages-->
+            </li>
+            </ul>
+        </li>
+        </ul>
+    </div>
+</div>
